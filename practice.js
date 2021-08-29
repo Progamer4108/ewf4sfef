d12830a69403f0e4e680a1fd9a43e624cef3f131
Progamer4108 <77099411+Progamer4108@users.noreply.github.com>
2021-08-29 16:45:40 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,21 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYb_VCyN7Zt5874VyJr2gDfNku_xXTo8U",
+    authDomain: "practice-a86a8.firebaseapp.com",
+    databaseURL: "https://practice-a86a8-default-rtdb.firebaseio.com",
+    projectId: "practice-a86a8",
+    storageBucket: "practice-a86a8.appspot.com",
+    messagingSenderId: "58718799225",
+    appId: "1:58718799225:web:b89648f8172a449f191ba8",
+    measurementId: "G-7CWTX2MBMF"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  function addUser() {
+      user_name = document.getElementById("user_name").value;
+      firebase.database().ref("/").child(user_name).update({
+       purpose : "add user"  
+      });
+  }
